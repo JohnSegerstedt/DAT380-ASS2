5972462ec3bef512e930381b686bb35824414cd9
JohnSegerstedt <sejohn@student.chalmers.se>
2020-11-14 09:21:32 +0100

    Adds John fans

---
+++ Assignment2/Assets/Scripts/FanAirCollision.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanAirCollision: MonoBehaviour{
+
+	private float airForce = 20f;
+	private Vector2 direction;
+
+	private void Start() {
+		float rotation = GetComponentInParent<Transform>().rotation.eulerAngles.z;
+		direction = new Vector2((float) -Math.Cos(rotation*Mathf.PI/180f), (float) -Math.Sin(rotation*Mathf.PI/180f));
+		Debug.Log(direction);
+	}
+	
+
+	private void OnTriggerStay2D(Collider2D collider) {
+		try{
+			collider.GetComponent<Rigidbody2D>().AddForce(direction * airForce);
+		}
+		catch(Exception){}
+
+	}
+}
---
+++ Assignment2/Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour{ 
+
+	[SerializeField] private Sprite[] frameArray;
+	private int currentFrame;
+	private float timer;
+
+	[SerializeField] private float timePerFrame = 0;
+
+	private SpriteRenderer spriteRenderer;
+
+	private void Start() {
+	if(timePerFrame == 0) timePerFrame = 0.1f;
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update(){
+        timer += Time.deltaTime;
+
+		if(timer >= timePerFrame){
+			timer -= timePerFrame;
+			currentFrame++;
+			if(currentFrame >= frameArray.Length){
+				currentFrame = 0;
+			}
+			spriteRenderer.sprite = frameArray[currentFrame];
+		}
+    }
+}
